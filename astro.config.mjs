84d5ae4c13e04ccc214d5df94c82e6119145c1b8
Mads Collin <madsc@hey.com>
2022-10-25 13:57:06 +0200

    more signature

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,14 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), alpinejs()],
+    integrations: [
+        tailwind({
+            // Example: Disable injecting a basic `base.css` import on every page.
+            // Useful if you need to define and/or import your own custom `base.css`.
+            config: { applyBaseStyles: true },
+        }),
+        alpinejs(),
+    ],
     site: "https://madscollin.github.io",
     base: "/sl-static",
 });
